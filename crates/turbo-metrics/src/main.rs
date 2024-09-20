3e5a842f366829f0ee9057d6656d1ce9e682e90b
Guillaume Anthouard <guillaume.anthouard@hotmail.fr>
2024-09-20 22:22:07 +0200

    turbo metrics, finally, after 1 year

--- crates/turbo-metrics/src/main.rs
+++ crates/turbo-metrics/src/main.rs
@@ -1,23 +1,52 @@
-use std::fmt::Display;
-use std::fs::File;
-use std::io::BufReader;
-use std::time::Instant;
-
+use clap::Parser;
 use codec_bitstream::h264::{ColourPrimaries, MatrixCoefficients, TransferCharacteristic};
 use codec_bitstream::{av1, h264};
 use cudarse_npp::get_stream_ctx;
+use cudarse_npp::image::ist::{PSNR, SSIM, WMSSSIM};
 use cudarse_npp::image::isu::Malloc;
 use cudarse_npp::image::{Image, ImgMut, C};
 use cudarse_video::dec::npp::NvDecFrame;
 use cudarse_video::sys::CUVIDEOFORMAT;
 use matroska_demuxer::{Frame, MatroskaFile};
+use ssimulacra2_cuda::Ssimulacra2;
+use stats::full::Stats;
+use std::fmt::Display;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::{Path, PathBuf};
+use std::time::Instant;
 use turbo_metrics::cudarse_driver::{CuDevice, CuStream};
 use turbo_metrics::cudarse_video::dec::npp::Img;
 use turbo_metrics::cudarse_video::dec::{CuVideoParser, CuvidParserCallbacks};
 use turbo_metrics::cudarse_video::dec_simple::NvDecoderSimple;
 use turbo_metrics::cudarse_video::sys::{cudaVideoCodec, cudaVideoCodec_enum};
 
+/// Turbo metrics compare the video tracks of two mkv files.
+#[derive(Parser, Debug)]
+#[command(version, author)]
+struct CliArgs {
+    /// Reference video muxed in mkv
+    reference: PathBuf,
+    /// Distorted video muxed in mkv
+    distorted: PathBuf,
+
+    /// Compute PSNR score
+    #[arg(long)]
+    psnr: bool,
+    /// Compute SSIM score
+    #[arg(long)]
+    ssim: bool,
+    /// Compute MSSSIM score
+    #[arg(long)]
+    msssim: bool,
+    /// Compute ssimulacra2 score
+    #[arg(long)]
+    ssimulacra2: bool,
+}
+
 fn main() {
+    let args = dbg!(CliArgs::parse());
+
     cudarse_driver::init_cuda().expect("Could not initialize the CUDA API");
     let dev = CuDevice::get(0).unwrap();
     println!(
@@ -35,9 +64,9 @@ fn main() {
         let cb_ref = NvDecoderSimple::new(3, None);
         let cb_dis = NvDecoderSimple::new(3, None);
 
-        let mut demuxer_ref = DemuxerParser::new("data/friends_cut.mkv", &cb_ref);
+        let mut demuxer_ref = DemuxerParser::new(&args.reference, &cb_ref);
         // let mut demuxer_dis = DemuxerParser::new("data/dummy_encode2.mkv", &cb_dis);
-        let mut demuxer_dis = DemuxerParser::new("data/encode0.mkv", &cb_dis);
+        let mut demuxer_dis = DemuxerParser::new(&args.distorted, &cb_dis);
 
         let mut counter = 0;
 
@@ -57,17 +86,69 @@ fn main() {
 
         assert_eq!(size, format_dis.size());
 
-        let main_stream = CuStream::new().unwrap();
-        let alt_stream = CuStream::new().unwrap();
-        main_stream.sync().unwrap();
-        alt_stream.sync().unwrap();
-        cudarse_npp::set_stream(main_stream.inner() as _).unwrap();
+        let streams = [(); 5].map(|()| CuStream::new().unwrap());
+        cudarse_npp::set_stream(streams[0].inner() as _).unwrap();
         let npp = get_stream_ctx().unwrap();
 
         let mut linear_rgb_ref = Image::malloc(size.width as _, size.height as _).unwrap();
         let mut linear_rgb_dis = linear_rgb_ref.malloc_same_size().unwrap();
-        let mut ref_8bit = linear_rgb_ref.malloc_same_size().unwrap();
-        let mut dis_8bit = linear_rgb_ref.malloc_same_size().unwrap();
+
+        let (mut ref_8bit, mut dis_8bit) = if args.psnr || args.ssim || args.msssim {
+            (
+                Some(linear_rgb_ref.malloc_same_size().unwrap()),
+                Some(linear_rgb_ref.malloc_same_size().unwrap()),
+            )
+        } else {
+            (None, None)
+        };
+
+        let (mut scratch_psnr, mut scores_psnr) = if args.psnr {
+            println!("Initializing PSNR");
+            (
+                Some(ref_8bit.as_ref().unwrap().psnr_alloc_scratch(npp).unwrap()),
+                Some(Vec::with_capacity(4096)),
+            )
+        } else {
+            (None, None)
+        };
+
+        let (mut scratch_ssim, mut scores_ssim) = if args.ssim {
+            println!("Initializing SSIM");
+            (
+                Some(ref_8bit.as_ref().unwrap().ssim_alloc_scratch(npp).unwrap()),
+                Some(Vec::with_capacity(4096)),
+            )
+        } else {
+            (None, None)
+        };
+
+        let (mut scratch_msssim, mut scores_msssim) = if args.msssim {
+            println!("Initializing MSSSIM");
+            (
+                Some(
+                    ref_8bit
+                        .as_ref()
+                        .unwrap()
+                        .wmsssim_alloc_scratch(npp)
+                        .unwrap(),
+                ),
+                Some(Vec::with_capacity(4096)),
+            )
+        } else {
+            (None, None)
+        };
+
+        let (mut ssimu, mut scores_ssimu) = if args.ssimulacra2 {
+            println!("Initializing SSIMULACRA2");
+            (
+                Some(Ssimulacra2::new(&linear_rgb_ref, &linear_rgb_dis, &streams[0]).unwrap()),
+                Some(Vec::with_capacity(4096)),
+            )
+        } else {
+            (None, None)
+        };
+
+        streams[0].sync().unwrap();
 
         println!("Initialized, now processing ...");
         let start = Instant::now();
@@ -83,11 +164,11 @@ fn main() {
                 if let (Some(fref), Some(fdis)) = (fref, fdis) {
                     let format = cb_ref.format().unwrap();
                     convert_frame_to_linearrgb(
-                        cb_ref.map_npp(&fref, &main_stream).unwrap(),
+                        cb_ref.map_npp(&fref, &streams[0]).unwrap(),
                         format,
                         &colorspace,
                         &mut linear_rgb_ref,
-                        &main_stream,
+                        &streams[0],
                     );
 
                     // if counter == 130 || counter == 200 || counter == 250 || counter == 300 {
@@ -97,22 +178,87 @@ fn main() {
 
                     let format = cb_dis.format().unwrap();
                     convert_frame_to_linearrgb(
-                        cb_dis.map_npp(&fdis, &alt_stream).unwrap(),
+                        cb_dis.map_npp(&fdis, &streams[1]).unwrap(),
                         format,
                         &colorspace,
                         &mut linear_rgb_dis,
-                        &alt_stream,
+                        &streams[1],
                     );
 
-                    colorspace
-                        .rgb_f32_to_8bit(&linear_rgb_ref, &mut ref_8bit, &main_stream)
-                        .unwrap();
-                    colorspace
-                        .rgb_f32_to_8bit(&linear_rgb_dis, &mut dis_8bit, &alt_stream)
-                        .unwrap();
+                    let mut psnr = 0.0;
+                    let mut ssim = 0.0;
+                    let mut msssim = 0.0;
 
-                    main_stream.wait_for_stream(&alt_stream).unwrap();
-                    main_stream.sync().unwrap();
+                    if let (Some(ref_8bit), Some(dis_8bit)) = (&mut ref_8bit, &mut dis_8bit) {
+                        streams[2].wait_for_stream(&streams[0]).unwrap();
+                        streams[3].wait_for_stream(&streams[1]).unwrap();
+                        colorspace
+                            .rgb_f32_to_8bit(&linear_rgb_ref, &mut *ref_8bit, &streams[2])
+                            .unwrap();
+                        colorspace
+                            .rgb_f32_to_8bit(&linear_rgb_dis, &mut *dis_8bit, &streams[3])
+                            .unwrap();
+
+                        streams[2].wait_for_stream(&streams[3]).unwrap();
+
+                        if args.psnr {
+                            streams[4].wait_for_stream(&streams[2]).unwrap();
+                            ref_8bit
+                                .psnr_into(
+                                    &mut *dis_8bit,
+                                    scratch_psnr.as_mut().unwrap(),
+                                    &mut psnr,
+                                    npp.with_stream(streams[4].inner() as _),
+                                )
+                                .unwrap();
+                        }
+                        if args.ssim {
+                            streams[3].wait_for_stream(&streams[2]).unwrap();
+                            ref_8bit
+                                .ssim_into(
+                                    &mut *dis_8bit,
+                                    scratch_ssim.as_mut().unwrap(),
+                                    &mut ssim,
+                                    npp.with_stream(streams[3].inner() as _),
+                                )
+                                .unwrap();
+                        }
+                        if args.msssim {
+                            ref_8bit
+                                .wmsssim_into(
+                                    dis_8bit,
+                                    scratch_msssim.as_mut().unwrap(),
+                                    &mut msssim,
+                                    npp.with_stream(streams[2].inner() as _),
+                                )
+                                .unwrap();
+                        }
+                    }
+
+                    if let Some(ssimu) = &mut ssimu {
+                        streams[0].wait_for_stream(&streams[1]).unwrap();
+                        ssimu.compute(&streams[0]).unwrap();
+                    }
+
+                    streams[0].wait_for_stream(&streams[1]).unwrap();
+                    streams[0].wait_for_stream(&streams[2]).unwrap();
+                    streams[0].wait_for_stream(&streams[3]).unwrap();
+                    streams[0].wait_for_stream(&streams[4]).unwrap();
+
+                    streams[0].sync().unwrap();
+
+                    if let Some(scores_psnr) = &mut scores_psnr {
+                        scores_psnr.push(psnr as f64);
+                    }
+                    if let Some(scores_ssim) = &mut scores_ssim {
+                        scores_ssim.push(ssim as f64);
+                    }
+                    if let Some(scores_msssim) = &mut scores_msssim {
+                        scores_msssim.push(msssim as f64);
+                    }
+                    if let Some(scores_ssimu) = &mut scores_ssimu {
+                        scores_ssimu.push(ssimu.as_mut().unwrap().get_score());
+                    }
 
                     counter += 1;
                 } else {
@@ -128,8 +274,19 @@ fn main() {
             total_frames,
             total_frames as u128 * 1000 / total
         );
-        // let stats = turbo.stats();
-        // println!("Stats : {stats:#?}");
+        println!("Stats :");
+        if let Some(scores) = &scores_psnr {
+            println!("  psnr: {:?}", Stats::compute(scores));
+        }
+        if let Some(scores) = &scores_ssim {
+            println!("  ssim: {:?}", Stats::compute(scores));
+        }
+        if let Some(scores) = &scores_msssim {
+            println!("  msssim: {:?}", Stats::compute(scores));
+        }
+        if let Some(scores) = &scores_ssimu {
+            println!("  ssimulacra2: {:?}", Stats::compute(scores));
+        }
     }
 
     dev.release_primary_ctx().unwrap();
@@ -146,7 +303,7 @@ struct DemuxerParser<'dec> {
 }
 
 impl<'dec> DemuxerParser<'dec> {
-    fn new(file: &str, dec: &'dec impl CuvidParserCallbacks) -> Self {
+    fn new(file: impl AsRef<Path>, dec: &'dec impl CuvidParserCallbacks) -> Self {
         let mkv = MatroskaFile::open(BufReader::new(File::open(file).unwrap())).unwrap();
 
         let (id, v_track) = mkv
@@ -246,7 +403,7 @@ fn mkv_codec_id_to_nvdec(id: &str) -> Option<cudaVideoCodec> {
 
 fn video_format_line(format: &CUVIDEOFORMAT) -> impl Display {
     format!(
-        "CP {:?}, TC {:?}, MC {:?}, Full range: {}",
+        "CP: {:?}, TC: {:?}, MC: {:?}, Full range: {}",
         ColourPrimaries::from_byte(format.video_signal_description.color_primaries,),
         TransferCharacteristic::from_byte(format.video_signal_description.transfer_characteristics,),
         MatrixCoefficients::from_byte(format.video_signal_description.matrix_coefficients,),
